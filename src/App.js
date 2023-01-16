d718616f6446949a16f0d464d31ed959fd9713a6
SeanIlagan123 <seanilagan135@gmail.com>
2023-01-16 16:37:34 +1100

    Fixed configuration with deployment issues with github pages. Will no longer use API calls for displaying Github repositories since it is security risk if I intend to use any authentication. It will be hard coded now.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
 import About from "./components/About";
@@ -9,15 +8,7 @@ import Contact from "./components/Contact";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/about" element={<About/>} />
-                <Route path="/skills" element={<Skills/>} />
-                <Route path="/projects" element={<Projects/>} />
-                <Route path="/contact" element={<Contact/>} />
-            </Routes>
-        </Router>
+        <Home />
     )
 }
 
